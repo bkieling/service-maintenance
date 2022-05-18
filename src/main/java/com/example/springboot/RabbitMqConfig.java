ee21f251b159fdd7e1cbc977489432687d70ec0a
fcaroline <fcaroline@vmware.com>
2022-05-18 10:25:09 +0200

    Adding ConnectionFactory JSON convertor

--- src/main/java/com/example/springboot/RabbitMqConfig.java
+++ src/main/java/com/example/springboot/RabbitMqConfig.java
@@ -1,6 +1,9 @@
 package com.example.springboot;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +13,18 @@ public class RabbitMqConfig {
     @Bean
     public Queue rfaUploadedQueue() {
         return new Queue("rfa.uploaded");
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
