290d5551b75995b3ea19ee46628b1ea2af035568
fcaroline <fcaroline@vmware.com>
2022-05-19 16:29:33 +0200

    Added button to send rfa and some controller tests

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,14 +3,22 @@ package com.example.springboot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
+	final RfaController rfaController;
+
 	@Value("${web.message}")
 	private String message;
 
+	public HelloController(RfaController rfaController) {
+		this.rfaController = rfaController;
+	}
 
 //	@RequestMapping("/")
 //	public String index() {
@@ -20,6 +28,12 @@ public class HelloController {
 	@RequestMapping("/")
 	public String home(Model model) {
 		model.addAttribute("message", message);
+		return "index";
+	}
+
+	@PostMapping("/rfa")
+	public String uploadRFA(@RequestParam(name="rfaContent", required=false, defaultValue="Some RFA content")String rfaContent) {
+		rfaController.uploadRfa(rfaContent);
 		return "index";
 	}
 }
